81640493368b624777ac626b8ce57c3ca2e57dd8
Valdo <vteixeiraromao@gmail.com>
2024-03-04 15:10:36 +0000

    Fixed collisions and roofs toggle

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,38 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let isHidden: boolean = false;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('entranceHall').subscribe(toggleRoof)
-    WA.room.area.onEnter('entranceLib').subscribe(toggleRoof)
-
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
+
+        listenDoor('hallDoor')
+        listenDoor('libraryDoor')
+        listenDoor('bankDoor')
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
 
-function toggleRoof() {
-    isHidden = !isHidden
-
-    if (isHidden) {
-        WA.room.hideLayer('Roof3')
-        WA.room.hideLayer('Roof2')
-        WA.room.hideLayer('Roof1')
-        WA.room.hideLayer('Roof0')
-    } else {
-        WA.room.hideLayer('Roof3')
-        WA.room.showLayer('Roof2')
-        WA.room.showLayer('Roof1')
-        WA.room.showLayer('Roof0')
-    }
-    
+function listenDoor(door: string) {
+    WA.room.area.onEnter(door).subscribe(() => {
+        if (WA.state.isRoofVisible === true) {
+            WA.state.isRoofVisible = false
+            WA.room.hideLayer('Roof/Roof3')
+            WA.room.hideLayer('Roof/Roof2')
+            WA.room.hideLayer('Roof/Roof1')
+            WA.room.hideLayer('Roof/Roof0')
+        } else {
+            WA.state.isRoofVisible = true
+            WA.room.showLayer('Roof/Roof3')
+            WA.room.showLayer('Roof/Roof2')
+            WA.room.showLayer('Roof/Roof1')
+            WA.room.showLayer('Roof/Roof0')
+        }
+    })
 }
 
 export {};
